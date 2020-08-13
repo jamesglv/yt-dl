f0db05cc294d6fe4bcf3fab83fc04e25f960ca2c
James Ogilvie <jamesogilvie@Jamess-Mac-mini.local>
2020-08-13 13:41:33 +1000

    Changed Port

--- Server/index.js
+++ Server/index.js
@@ -4,10 +4,11 @@ const ytdl = require('ytdl-core');
 const app = express();
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
+var PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log("Server works");
 });
 
