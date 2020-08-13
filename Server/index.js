a3b63bc2e2f839aa90213ccf4548d69db7edfe99
James Ogilvie <jamesogilvie@Jamess-Mac-mini.local>
2020-08-13 13:37:36 +1000

    ready for heroku

---
+++ Server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+
+app.use(cors());
+
+app.listen(4000, () => {
+    console.log("Server works");
+});
+
+app.get('/download', (req,res) => {
+    var URL = req.query.URL;
+
+    if (req.query.output === 'video') {
+        res.header('Content-Disposition', 'attachment; filename="download.mp4"');
+        ytdl(URL, {
+            format: 'mp4'
+        }).pipe(res);
+    } 
+    else if (req.query.output === 'audio') {
+        res.header('Content-Disposition', 'attachment;');
+        var stream = ytdl(URL, {
+            format: 'mp4'
+        });
+    
+        var command = ffmpeg(stream)
+        .noVideo()
+        .audioCodec('libmp3lame')
+        .on('end', function(){
+            console.log('finished')
+        })
+        .format('mp3')
+        .pipe(res)
+    }
+});
---
+++ script.js
@@ -0,0 +1,14 @@
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+var outputSelect = document.querySelector('.output-select');
+
+convertBtn.addEventListener('click', () => {
+    console.log("URL: "+URLinput.value);
+    console.log(outputSelect.value);
+    sendURL(URLinput.value, outputSelect.value);
+});
+
+function sendURL(URL,outSel) {
+    window.location.href = "http://localhost:4000/download?URL="+URL+"&output="+outSel;
+    console.log("http://localhost:4000/download?URL="+URL+"&output="+outSel)
+}
